383b0c917645cac6e60c916ef68304cfef341348
Alessandro <adgrimaldi96@gmail.com>
2017-05-05 13:23:16 -0600

    Menu terminado

---
+++ alessandro_lab3.cpp
@@ -0,0 +1,26 @@
+nclude <iostream>
+
+#include <fstream>
+
+#include <string>
+
+using namespace std;
+
+int main(){
+	int resp = 's';
+	int opcion;
+	while(resp=='s'||resp=='S')
+		cout<<"MENU:\n"<<"1) Ejercicio 1\n"<<"2) Ejercicio 2\n"<<"3) Salir"<<endln;
+		cin>>opcion;
+		
+		if(opcion==1){
+	}//if opcion 1
+		if(opcion==2){
+	}//if opcion 2
+
+
+	cout<<"Desea continuar[s/n]: "<<endl;
+	cin>>resp;
+
+ }//fin while
+}//fin main
